ec0a06570afa4f87d9aafafa6d57bcd4f5e9d087
月小泪 <york.yang.me@gmail.com>
2024-03-21 15:26:25 +0900

    Update RevertList algorithm

---
+++ linkedList/RevertList.go
@@ -0,0 +1,44 @@
+package linkedlist
+
+// 双链表求解
+/**
+步骤：
+	保存 pHead 的下一个节点，防止后续链表丢失
+	将 pHead 指向 newHead，完成节点反转
+	更新 newHead 为 pHead，将完成反转的节点加入链表
+	更新 pHead，继续反转还未反转的链表
+**/
+/*
+ * type ListNode struct{
+ *   Val int
+ *   Next *ListNode
+ * }
+ */
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 当前反转节点
+ * 已反转的链表
+ * 未反转的链表
+ * @param pHead ListNode类
+ * @return ListNode类
+ */
+func ReverseList(pHead *ListNode) *ListNode {
+	// write code here
+	if pHead == nil || pHead.Next == nil {
+		return pHead
+	}
+	var newHead *ListNode
+	// for pHead != nil {
+	// 	pNext := pHead.Next
+	// 	pHead.Next = newHead
+	// 	newHead = pHead
+	// 	pHead.Next = pNext
+	// }
+	//	平行赋值语法
+	for pHead != nil {
+		pHead, pHead.Next, newHead = pHead.Next, newHead, pHead
+	}
+	return newHead
+}
