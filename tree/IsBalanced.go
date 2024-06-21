692bdeeab6ecf59f7378ac8f9be72179b16288ce
月小泪 <york.yang.me@gmail.com>
2024-06-21 13:40:50 +0900

    Add IsBalanced algorithm

---
+++ tree/IsBalanced.go
@@ -0,0 +1,41 @@
+package tree
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param pRoot TreeNode类
+ * @return bool布尔型
+ */
+func IsBalanced_Solution(pRoot *TreeNode) bool {
+	// write code here
+	res := true
+	var dfs func(node *TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		left := dfs(node.Left)
+		right := dfs(node.Right)
+		if abs(left, right) > 1 {
+			res = false
+		}
+		return max(left, right) + 1
+	}
+	dfs(pRoot)
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func abs(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
