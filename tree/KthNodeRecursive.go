bd8644be88f121ab835506d77dd352a82af34e57
月小泪 <york.yang.me@gmail.com>
2024-07-05 16:44:05 +0900

    Add algorithm realization steps

--- tree/KthNodeRecursive.go
+++ tree/KthNodeRecursive.go
@@ -1,5 +1,12 @@
 package tree
 
+/**
+算法实现步骤
+step 1：设置全局变量count记录遍历了多少个节点，res记录第k个节点。
+step 2：另写一函数进行递归中序遍历，当节点为空或者超过k时，结束递归，返回。
+step 3：优先访问左子树，再访问根节点，访问时统计数字，等于k则找到。
+step 4：最后访问右子树。
+**/
 var index int
 
 func KthNodeRecursive(proot *TreeNode, k int) int {
