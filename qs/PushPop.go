0da1ab68aec659f73ebee0256b1eec049eb7fd90
月小泪 <york.yang.me@gmail.com>
2024-09-25 13:43:32 +0900

    Add Push pop for queue algorithm

---
+++ qs/PushPop.go
@@ -0,0 +1,20 @@
+package qs
+
+var stack1 []int
+var stack2 []int
+
+func Push(node int) {
+	stack1 = append(stack1, node)
+}
+
+func Pop() int {
+	if len(stack2) == 0 {
+		for i := len(stack1) - 1; i >= 0; i-- {
+			stack2 = append(stack2, stack1[i])
+		}
+		stack1 = []int{}
+	}
+	res := stack2[len(stack2)-1]
+	stack2 = stack2[:len(stack2)-1]
+	return res
+}
