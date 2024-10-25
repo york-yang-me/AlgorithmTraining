803e46738e282ce68f6e5c693c573f61628cbe62
月小泪 <york.yang.me@gmail.com>
2024-10-25 20:02:52 +0800

    Add FirstNotRepeatingChar algorithm

---
+++ writtenTest/FirstNotRepeatingChar.go
@@ -0,0 +1,16 @@
+package writtentest
+
+func FirstNotRepeatingChar(str string) int {
+	m := map[string]int{}
+	for i := 0; i < len(str); i++ {
+		tmp := string(str[i])
+		m[tmp] += 1
+	}
+	for i := 0; i < len(str); i++ {
+		tmp := string(str[i])
+		if m[tmp] == 1 {
+			return i
+		}
+	}
+	return -1
+}
